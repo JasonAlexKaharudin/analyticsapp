7524b9e22064b51f88c71e5a3cacd3ed87e79600
Jason <jasonakaharudin@gmail.com>
2023-07-26 18:25:09 +0800

    filter by startDate is done for buttonClick

---
+++ vite-app/server/middleware/validateStartDate.js
@@ -0,0 +1,18 @@
+import { query, validationResult } from 'express-validator';
+
+const validateStartDate = [
+    query('startDate')
+        .optional()
+        .isISO8601()
+        .withMessage('startDate must be a valid ISO8601 date'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const errorMessages = errors.array().map(error => error.msg);
+            return res.status(422).json({ error: errorMessages });
+        }
+        next();
+    },
+];
+
+export default validateStartDate;
--- vite-app/server/routes/buttonClicks.js
+++ vite-app/server/routes/buttonClicks.js
@@ -1,8 +1,10 @@
 import express from "express";
 import createValidationMiddleware from "../middleware/validationMiddleWare.js";
+import validateStartDate from "../middleware/validateStartDate.js";
 import ButtonClick from "../models/ButtonClick.js";
 
 const router = express.Router();
+const defualtDays = 7;
 
 const buttonClickValidationMiddleware = createValidationMiddleware('ButtonClick');
 router.post("/button-click", buttonClickValidationMiddleware, async (req, res) => {
@@ -14,6 +16,28 @@ router.post("/button-click", buttonClickValidationMiddleware, async (req, res) =
         res.status(201).json(createdButtonClickObject);
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+
+router.get("/button-clicks", validateStartDate, async (req, res) => {
+    try{
+        const startDate = new Date(req.query.startDate || Date.now() - defualtDays * 24 * 60 * 60 * 1000);
+
+        const buttonClicks = await ButtonClick.aggregate([
+            {
+              $unwind: '$clicks',
+            },
+            {
+              $match: {
+                'clicks.timestamp': { $gte: startDate },
+              },
+            },
+          ]);
+
+        res.status(200).json(buttonClicks);
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 })
 
