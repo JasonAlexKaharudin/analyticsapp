0ab687cc6d3da0753101bc58d9603b55c30f25d6
Jason Kaharudin <jasonakaharudin@gmail.com>
2023-09-16 22:53:36 +0800

    with eslint coding style

--- vite-app/server/routes/pageView.js
+++ vite-app/server/routes/pageView.js
@@ -1,64 +1,64 @@
-import express from "express";
-import createValidationMiddleware from "../middleware/validationMiddleWare.js";
-import validateStartDate from "../middleware/validateStartDate.js";
-import PageViewDuration from "../models/PageViewDuration.js";
+import express from 'express'
+import createValidationMiddleware from '../middleware/validationMiddleWare.js'
+import validateStartDate from '../middleware/validateStartDate.js'
+import PageViewDuration from '../models/PageViewDuration.js'
 
-const router = express.Router();
-const defualtDays = 7;
+const router = express.Router()
+const defualtDays = 7
 
-const pageViewValidationMiddleware = createValidationMiddleware('PageViewDuration');
-router.post("/page-view", pageViewValidationMiddleware, async (req, res) => {
-    try {
-        const pageViewDurationData = req.body;
-        const createdPageViewObject = await PageViewDuration.create(pageViewDurationData)
+const pageViewValidationMiddleware = createValidationMiddleware('PageViewDuration')
+router.post('/page-view', pageViewValidationMiddleware, async (req, res) => {
+  try {
+    const pageViewDurationData = req.body
+    const createdPageViewObject = await PageViewDuration.create(pageViewDurationData)
 
-        console.log("Successful created Page View");
-        res.status(201).json(createdPageViewObject);
-    } catch(error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-router.get("/average-page-views", validateStartDate, async (req, res) => {
-    try{
-        const startDate = new Date(req.query.startDate || Date.now() - defualtDays * 24 * 60 * 60 * 1000);
-
-        const buttonClicks = await PageViewDuration.aggregate([
-            {
-                $match: {
-                  timestamp: { $gte: startDate },
-                },
-            },
-            {
-                $group: {
-                  _id: '$pathURL',
-                  averageDuration: { $avg: '$duration' },
-                },
-            },
-            {
-                $project: {
-                    pageURL: '$_id',
-                    _id: 0,
-                    averageDuration: { $round: ['$averageDuration', 2] },
-                },
-            },
-            {
-                $sort: {
-                    averageDuration: 1,
-                },
-            },
-          ]);
-
-        const responsePayload = {
-          totalObjects: buttonClicks.length,
-          data: buttonClicks.reverse()
-        }
-
-        res.status(200).json(responsePayload);
-    } catch(error) {
-        console.log(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+    console.log('Successful created Page View')
+    res.status(201).json(createdPageViewObject)
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
 })
 
-export default router;
+router.get('/average-page-views', validateStartDate, async (req, res) => {
+  try {
+    const startDate = new Date(req.query.startDate || Date.now() - defualtDays * 24 * 60 * 60 * 1000)
+
+    const buttonClicks = await PageViewDuration.aggregate([
+      {
+        $match: {
+          timestamp: { $gte: startDate }
+        }
+      },
+      {
+        $group: {
+          _id: '$pathURL',
+          averageDuration: { $avg: '$duration' }
+        }
+      },
+      {
+        $project: {
+          pageURL: '$_id',
+          _id: 0,
+          averageDuration: { $round: ['$averageDuration', 2] }
+        }
+      },
+      {
+        $sort: {
+          averageDuration: 1
+        }
+      }
+    ])
+
+    const responsePayload = {
+      totalObjects: buttonClicks.length,
+      data: buttonClicks.reverse()
+    }
+
+    res.status(200).json(responsePayload)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+
+export default router
